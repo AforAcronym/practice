3b7d03e203d8ada1e5ae784041302bd2952ce19d
AforAcronym <ppq@hyperboria.ca>
2013-09-30 13:17:32 +0400

    Partly made HashTable and modified Array

--- src/practice3/Array.java
+++ src/practice3/Array.java
@@ -2,137 +2,158 @@ package practice3;
 
 /**
  * Created with IntelliJ IDEA. Continued with Eclipse. by E. Shevchenko
- * 
  */
 public class Array<E> {
 
-	private E[] array;
-	int lastPos = 0;
+    int lastPos = 0;
+    private E[] array;
 
-	/**************************************************************************
-	 * Constructor
-	 * 
-	 * @param size
-	 */
-	@SuppressWarnings("unchecked")
-	public Array(int size) {
-		array = (E[]) new Object[size];
-	}
+    /**
+     * ***********************************************************************
+     * Constructor
+     *
+     * @param size
+     */
+    @SuppressWarnings("unchecked")
+    public Array(int size) {
+        array = (E[]) new Object[size];
+    }
 
-	/**************************************************************************
-	 * Returns size
-	 * 
-	 * @return
-	 */
-	public int getSize() {
-		return array.length;
-	}
+    /**
+     * ***********************************************************************
+     * Returns size
+     *
+     * @return
+     */
+    public int getSize() {
+        return array.length;
+    }
 
-	/**************************************************************************
-	 * Check whether the index is within array boundaries
-	 * 
-	 * @param index
-	 * @return
-	 */
-	private boolean indexOK(int index) {
-		return index < array.length && index >= 0;
-	}
+    /**
+     * ***********************************************************************
+     * Check whether the index is within array boundaries
+     *
+     * @param index
+     * @return
+     */
+    private boolean indexOK(int index) {
+        return index < array.length && index >= 0;
+    }
 
-	/**************************************************************************
-	 * If index is within array boundary, returns Element at specified index,
-	 * otherwise returns null
-	 * 
-	 * @param index
-	 * @return
-	 */
-	public E getElement(int index) {
-		if (indexOK(index)) {
-			return (E) array[index];
-		}
-		return null;
-	}
+    /**
+     * ***********************************************************************
+     * If index is within array boundary, returns Element at specified index,
+     * otherwise returns null
+     *
+     * @param index
+     * @return
+     */
+    public E getElement(int index) {
+        if (indexOK(index)) {
+            return (E) array[index];
+        }
+        return null;
+    }
 
-	/**************************************************************************
-	 * If index is within array boundary, places specified element at specified
-	 * position and returns true, otherwise returns false
-	 * 
-	 * @param index
-	 * @param element
-	 * @return
-	 */
-	public boolean setElement(int index, E element) {
-		if (indexOK(index)) {
-			
-			array[index] = element;
-			
-			if (index > lastPos && element != null) {
-				lastPos = index;
-			} else if ( index == lastPos && element == null ) {
-				while (array[lastPos] == null && lastPos > 0) {
-					--lastPos;
-				}
-			}
-			return true;
-		}
-		return false;
-	}
+    /**
+     * ***********************************************************************
+     * If index is within array boundary, places specified element at specified
+     * position and returns true, otherwise returns false
+     *
+     * @param index
+     * @param element
+     * @return
+     */
+    public boolean setElement(int index, E element) {
+        if (indexOK(index)) {
 
-	/**************************************************************************
-	 * Change capacity by the passed number (may be negative)
-	 * 
-	 * @param number
-	 */
-	@SuppressWarnings("unchecked")
-	public void changeCapacityBy(int number) {
-		int newSize = array.length + number;
-		E[] temparray = (E[]) new Object[newSize];
-		System.arraycopy(array, 0, temparray, 0, array.length > newSize ? newSize : array.length);
-		array = temparray;
-	}
-	
-	/**************************************************************************
-	 * Change capacity by the passed number (may be negative)
-	 * 
-	 * @param number
-	 */
-	public Array<E> getCopy() {
-		Array<E> newArr = new Array<E>(array.length);
-		System.arraycopy(array, 0, newArr.array, 0, array.length);
-		return newArr;
-	}
-	
-	/**************************************************************************
-	 * Shifts array content adding or removing cells at the beginning
-	 */
-	private void shiftBy(int number) {
-		E[] temparr = (E[]) new Object[array.length + number];
-		int srcPos = (number > 0) ? 0 : number;
-		int destPos = (number > 0) ? number : 0;
-		int length = (number > 0) ? array.length : array.length - number;
-		System.arraycopy(array, srcPos, temparr, destPos, length);
-		array = temparr;
-	}
-	
-	/**************************************************************************
-	 * Returns String
-	 */
-	public int getLastPosition() {
-		return lastPos;
-	}
+            array[index] = element;
 
-	/**************************************************************************
-	 * Returns String
-	 */
-	@Override
-	public String toString() {
-		StringBuffer sb = new StringBuffer();
-		for (int i = 0; i < array.length; i++) {
-			sb.append("Array[" + i + "] = " + array[i]);
-			if (i < array.length - 1) {
-				sb.append("\n");
-			}
-		}
-		return sb.toString();
-	}
+            if (index > lastPos && element != null) {
+                lastPos = index;
+            } else if (index == lastPos && element == null) {
+                while (array[lastPos] == null && lastPos > 0) {
+                    --lastPos;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * ***********************************************************************
+     * Append element to the end  of the array and expand capacity by one if needed
+     *
+     * @param element
+     * @return
+     */
+    public boolean append(E element) {
+        if (lastPos > array.length - 1) {
+            changeCapacityBy(1);
+        }
+        return setElement(lastPos, element);
+    }
+
+    /**
+     * ***********************************************************************
+     * Change capacity by the passed number (may be negative)
+     *
+     * @param number
+     */
+    @SuppressWarnings("unchecked")
+    public void changeCapacityBy(int number) {
+        int newSize = array.length + number;
+        E[] temparray = (E[]) new Object[newSize];
+        System.arraycopy(array, 0, temparray, 0, array.length > newSize ? newSize : array.length);
+        array = temparray;
+    }
+
+    /**
+     * ***********************************************************************
+     * Change capacity by the passed number (may be negative)
+     */
+    public Array<E> getCopy() {
+        Array<E> newArr = new Array<E>(array.length);
+        System.arraycopy(array, 0, newArr.array, 0, array.length);
+        return newArr;
+    }
+
+    /**
+     * ***********************************************************************
+     * Shifts array content adding or removing cells at the beginning
+     */
+    private void shiftBy(int number) {
+        E[] temparr = (E[]) new Object[array.length + number];
+        int srcPos = (number > 0) ? 0 : number;
+        int destPos = (number > 0) ? number : 0;
+        int length = (number > 0) ? array.length : array.length - number;
+        System.arraycopy(array, srcPos, temparr, destPos, length);
+        array = temparr;
+    }
+
+    /**
+     * ***********************************************************************
+     * Returns String
+     */
+    public int getLastPosition() {
+        return lastPos;
+    }
+
+    /**
+     * ***********************************************************************
+     * Returns String
+     */
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < array.length; i++) {
+            sb.append("Array[" + i + "] = " + array[i]);
+            if (i < array.length - 1) {
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
+    }
 
 }
--- src/practice3/HashTable.java
+++ src/practice3/HashTable.java
@@ -6,16 +6,22 @@ package practice3;
 public class HashTable<K extends Comparable<K>, V> {
 
     private double loadFactor = 0.75;
-    private int INITIAL_CAPACITY = 50;
-    private V[] buckets = (V[]) new Object[INITIAL_CAPACITY];
+    private double loadCounter = 0;
+    private int capacity = 50;
+    private int subArrayCapacity = 5;
+    // Array of Arrays of KeyValueHolders
+    private Array<Array<KeyValueHolder<K, V>>> buckets = new Array<Array<KeyValueHolder<K, V>>>(capacity);
 
     /**
-     * Hash    TODO
+     * Hash function   TODO
      */
     private int hash(final K key) {
         int res = 0;
-
+        // If string
+        // If number
+        // get length of number and nearest prime numbers to mod and to add
         return res;
+
     }
 
     /**
@@ -25,7 +31,9 @@ public class HashTable<K extends Comparable<K>, V> {
      * @param key
      * @return
      */
-    public Object get(Object key) {
+    public V get(K key) {
+        int bucketIndex = hash(key);
+        // TODO searchValue method?
         return null;
     }
 
@@ -38,8 +46,48 @@ public class HashTable<K extends Comparable<K>, V> {
      * @param value
      * @return
      */
-    public Object put(Object key, Object value) {
-        return null;
+    public V put(K key, V value) {
+
+        // Here is magic
+        int bucketIndex = hash(key);
+
+        // Previous value associated with the key
+        V prevVal = get(key);
+
+        // TODO searchValue method?
+        if (buckets.getElement(bucketIndex) == null) {
+            // There is nothing even with this key's hash
+
+            buckets.setElement(bucketIndex, new Array<KeyValueHolder<K, V>>(subArrayCapacity));
+            buckets.getElement(bucketIndex).append(new KeyValueHolder<K, V>(key, value));
+
+        } else {
+            // We have collision and need to find out if the key is in here
+
+            Array<KeyValueHolder<K, V>> subArray = buckets.getElement(bucketIndex);
+
+            for (int i = 0; i < subArray.getSize(); ++i) {
+                KeyValueHolder<K, V> holder = subArray.getElement(i);
+                // The key is found, wee need to change it's value
+                if (holder.key == key) {
+                    prevVal = holder.value;
+                    subArray.setElement(i, new KeyValueHolder<K, V>(key, value));
+                }
+            }
+
+            // The key is not associated with any value, so new key-value pair holder created
+            if (prevVal == null) {
+                buckets.getElement(bucketIndex).append(new KeyValueHolder<K, V>(key, value));
+            }
+        }
+
+        ++loadCounter;
+
+        if (loadCounter / capacity >= loadFactor) {
+            // Increase the hash capacity
+            buckets.changeCapacityBy(buckets.getSize() * 2);
+        }
+        return prevVal;
     }
 
     /**
@@ -49,9 +97,37 @@ public class HashTable<K extends Comparable<K>, V> {
      * @param key
      * @return
      */
-    public Object remove(Object key) {
+    public V remove(K key) {
+        // TODO
         return null;
 
+    }
+
+    /**
+     * String representation
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        // TODO
+        return "";
+    }
+
+    /**
+     * A special inner class for holding key-pair pairs
+     *
+     * @param <K> key
+     * @param <V> value
+     */
+    private class KeyValueHolder<K, V> {
+        private K key;
+        private V value;
+
+        public KeyValueHolder(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 
 }
