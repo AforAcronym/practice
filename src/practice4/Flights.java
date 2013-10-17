8eba17d4cf3302dd6a00f3beef93c9c9ff57599b
AforAcronym <ppq@hyperboria.ca>
2013-10-17 11:16:37 +0400

    Lecture on annotations, reflections and security

---
+++ src/practice4/Flights.java
@@ -0,0 +1,22 @@
+package practice4;
+
+import javax.swing.*;
+
+/**
+ * 4.	Create a Java Flights GUI. The window must consist of the following components:
+ * a.	Three text labels: “TO”, “FROM” and “WHEN”
+ * b.	Two text fields that allows to enter departure and arrival location names
+ * c.	One “formatted” text field (use JFormattedTextField class) that allows to enter
+ *      desired flight date
+ * d.	“Reset” button that clears all text fields
+ * e.	“Search” button that runs a search for a flight using given “to”, “from” and “when” information
+ * f.	Text area that is used to display search results. It must display information about every
+ *      flight that satisfies criteria specified in textboxes above.
+ */
+
+public class Flights extends JFrame {
+
+    public static void main(String[] args) {
+
+    }
+}
